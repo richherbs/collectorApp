61a5a92c1a2c59bf6be8470b0366372a7f3444f8
Richard Herbert <richard.herbert1980@gmail.com>
2020-03-23 15:49:30 +0000

    add frontend

---
+++ index.php
@@ -0,0 +1,70 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Bike Collector</title>
+    <link rel="stylesheet" href="styles/styles.css">
+    <link href="https://fonts.googleapis.com/css?family=Martel:200,300,400,600,700,800,900|Poppins:100,100i,200,200i,300,300i,400,400i,500,500i,600,600i,700,700i,800,800i,900,900i&display=swap" rel="stylesheet">
+</head>
+<body>
+    <h1>Bike Collector App</h1>
+    <div class='card-container'>
+        <div class='card'>
+            <img class='bike-image' src="images/sb130.jpeg" alt="Yeti sb130 bike">
+            <div class="card-info-container">
+                <section>Make: Yeti</section>
+                <section>Model: sb130</section>
+                <section>Discipline: Trail</section>
+                <section>Wheel Size: 29er</section>
+            </div>
+            <div class='card-button-container'>
+                <button>Edit Bike</button>
+                <button>Delete Bike</button>
+            </div>
+        </div>
+        <div class='card'>
+        <img class='bike-image' src="images/sb130.jpeg" alt="Yeti sb130 bike">
+            <div class="card-info-container">
+                <section>Make: Yeti</section>
+                <section>Model: sb130</section>
+                <section>Discipline: Trail</section>
+                <section>Wheel Size: 29er</section>
+            </div>
+            <div class='card-button-container'>
+                <button>Edit Bike</button>
+                <button>Delete Bike</button>
+            </div>
+        </div>
+        <div class='card'>
+        <img class='bike-image' src="images/sb130.jpeg" alt="Yeti sb130 bike">
+            <div class="card-info-container">
+                <section>Make: Yeti</section>
+                <section>Model: sb130</section>
+                <section>Discipline: Trail</section>
+                <section>Wheel Size: 29er</section>
+            </div>
+            <div class='card-button-container'>
+                <button>Edit Bike</button>
+                <button>Delete Bike</button>
+            </div>
+        </div>
+        <div class='card'>
+        <img class='bike-image' src="images/sb130.jpeg" alt="Yeti sb130 bike">
+            <div class="card-info-container">
+                <section>Make: Yeti</section>
+                <section>Model: sb130</section>
+                <section>Discipline: Trail</section>
+                <section>Wheel Size: 29er</section>
+            </div>
+            <div class='card-button-container'>
+                <button>Edit Bike</button>
+                <button>Delete Bike</button>
+            </div>
+        </div>
+    </div>
+    <form class='add-form' method="get">
+        <button class='button-add' type="submit">Add Bike</button>
+    </form>
+</body>
+</html>
