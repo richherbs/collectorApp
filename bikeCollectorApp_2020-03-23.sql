c4f11dfdbff76a4b916669e2575b715addbfe939
Richard Herbert <richard.herbert1980@gmail.com>
2020-03-23 16:18:48 +0000

    add db

---
+++ bikeCollectorApp_2020-03-23.sql
@@ -0,0 +1,84 @@
+# ************************************************************
+# Sequel Pro SQL dump
+# Version 4541
+#
+# http://www.sequelpro.com/
+# https://github.com/sequelpro/sequelpro
+#
+# Host: 127.0.0.1 (MySQL 5.7.29)
+# Database: bikeCollectorApp
+# Generation Time: 2020-03-23 16:17:48 +0000
+# ************************************************************
+
+
+/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
+/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
+/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
+/*!40101 SET NAMES utf8 */;
+/*!40014 SET @OLD_FOREIGN_KEY_CHECKS=@@FOREIGN_KEY_CHECKS, FOREIGN_KEY_CHECKS=0 */;
+/*!40101 SET @OLD_SQL_MODE=@@SQL_MODE, SQL_MODE='NO_AUTO_VALUE_ON_ZERO' */;
+/*!40111 SET @OLD_SQL_NOTES=@@SQL_NOTES, SQL_NOTES=0 */;
+
+
+# Dump of table bikes
+# ------------------------------------------------------------
+
+DROP TABLE IF EXISTS `bikes`;
+
+CREATE TABLE `bikes` (
+  `id` int(11) unsigned NOT NULL AUTO_INCREMENT,
+  `brand_ID` int(11) NOT NULL,
+  `model` varchar(11) NOT NULL DEFAULT '',
+  `discipline_ID` int(11) NOT NULL,
+  `wheelSize_ID` int(11) NOT NULL,
+  PRIMARY KEY (`id`)
+) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+
+
+
+# Dump of table brand
+# ------------------------------------------------------------
+
+DROP TABLE IF EXISTS `brand`;
+
+CREATE TABLE `brand` (
+  `id` int(11) unsigned NOT NULL AUTO_INCREMENT,
+  `brand_name` varchar(30) NOT NULL DEFAULT '',
+  PRIMARY KEY (`id`)
+) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+
+
+
+# Dump of table discipline
+# ------------------------------------------------------------
+
+DROP TABLE IF EXISTS `discipline`;
+
+CREATE TABLE `discipline` (
+  `id` int(11) unsigned NOT NULL AUTO_INCREMENT,
+  `discipline_name` int(11) NOT NULL,
+  PRIMARY KEY (`id`)
+) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+
+
+
+# Dump of table wheelSize
+# ------------------------------------------------------------
+
+DROP TABLE IF EXISTS `wheelSize`;
+
+CREATE TABLE `wheelSize` (
+  `id` int(11) unsigned NOT NULL AUTO_INCREMENT,
+  `wheel_diameter` int(11) NOT NULL,
+  PRIMARY KEY (`id`)
+) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+
+
+
+
+/*!40111 SET SQL_NOTES=@OLD_SQL_NOTES */;
+/*!40101 SET SQL_MODE=@OLD_SQL_MODE */;
+/*!40014 SET FOREIGN_KEY_CHECKS=@OLD_FOREIGN_KEY_CHECKS */;
+/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
+/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
+/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
